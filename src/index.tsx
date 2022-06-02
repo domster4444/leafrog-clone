e4f8bf4cb0654a4d9af7ec3e76087190d7a36127
domster4444 <kshitiz.shah@deerwalk.edu.np>
2022-06-02 05:59:17 -0500

    chore(config): setup sass folder structure

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+//? styles
+import 'styles/scss/main.min.css';
+
 /**
  * @description //*redux-toolkit imports
  */
